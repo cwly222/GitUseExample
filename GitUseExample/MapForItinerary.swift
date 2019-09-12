92e252078551e90834fb34c4b0fd078945e22feb
Cameron Lydon <cameron.lydon@uky.edu>
2019-09-12 13:28:42 -0400

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by  on 9/12/19.
+//  Copyright © 2019 Cameron Lydon. All rights reserved.
+//
+
+import UIKit
+
+class MapforItinerary: NSObject {
+    
+}
