8db180a9ab2b370b39ef37df568670325651d67f
Cameron Lydon <cameron.lydon@uky.edu>
2019-09-12 12:58:15 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by  on 9/12/19.
+//  Copyright © 2019 Cameron Lydon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
